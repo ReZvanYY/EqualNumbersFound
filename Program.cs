f7e581a6b7a5f741b351cf4a5db00cc506c5b4b8
ReZvanYY <141616463+ReZvanYY@users.noreply.github.com>
2023-11-17 14:28:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace OnePlusOneEqualsMaybeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            var setNumber = number.Next(0, 11);
+
+
+            Console.WriteLine("Give me the first number between 0 and 10, please!");
+            var numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me the second number between 0 and 10, please!");
+            var numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOne != numberTwo)
+            {
+                Console.WriteLine("The answer is:" + AddUnEquals(numberOne, numberTwo));
+            }
+            else if (numberOne == numberTwo)
+            {
+                Console.WriteLine("The answer is:" + MultiplyEquals(numberOne, numberTwo));
+            }
+        }
+
+        static int AddUnEquals(int NumberOne, int NumberTwo)
+        {
+            return NumberOne + NumberTwo;
+
+        }
+        static int MultiplyEquals(int NumberOne, int NumberTwo)
+        {
+            return NumberOne * NumberTwo;
+
+        }
+    }
+
+}
